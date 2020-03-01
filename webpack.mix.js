20a4cb7a9ef9a44fe259ad896d2bfbfef43ea273
Mutahhar Naseer <muthhar@yahoo.com>
2020-03-02 01:59:18 +0500

    installed laravel ui package.

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .postCss('resources/css/main.css', 'public/css', [
-       require('tailwindcss'),
-   ])
-   .browserSync('weather-laravel-vue.practice');
+   .sass('resources/sass/app.scss', 'public/css');
